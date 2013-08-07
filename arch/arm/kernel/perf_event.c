271bc1cf33490d496f0a8513d4fe51229113dd46
Will Deacon <will.deacon@arm.com>
2013-08-07 23:39:41 +0100

    ARM: 7809/1: perf: fix event validation for software group leaders
    commit c95eb3184ea1a3a2551df57190c81da695e2144b upstream.
    
    It is possible to construct an event group with a software event as a
    group leader and then subsequently add a hardware event to the group.
    This results in the event group being validated by adding all members
    of the group to a fake PMU and attempting to allocate each event on
    their respective PMU.
    
    Unfortunately, for software events wthout a corresponding arm_pmu, this
    results in a kernel crash attempting to dereference the ->get_event_idx
    function pointer.
    
    This patch fixes the problem by checking explicitly for software events
    and ignoring those in event validation (since they can always be
    scheduled). We will probably want to revisit this for 3.12, since the
    validation checks don't appear to work correctly when dealing with
    multiple hardware PMUs anyway.
    
    Reported-by: Vince Weaver <vincent.weaver@maine.edu>
    Tested-by: Vince Weaver <vincent.weaver@maine.edu>
    Tested-by: Mark Rutland <mark.rutland@arm.com>
    Signed-off-by: Will Deacon <will.deacon@arm.com>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- arch/arm/kernel/perf_event.c
+++ arch/arm/kernel/perf_event.c
@@ -350,6 +350,9 @@ validate_event(struct pmu *pmu, struct pmu_hw_events *hw_events,
 	if (is_software_event(event))
 		return 1;
 
+	if (event->pmu != leader_pmu || event->state < PERF_EVENT_STATE_OFF)
+		return 1;
+
 	/*
 	 * Reject groups spanning multiple HW PMUs (e.g. CPU + CCI). The
 	 * core perf code won't check that the pmu->ctx == leader->ctx
